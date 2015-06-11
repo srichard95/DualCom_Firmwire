8b34c649884732a78095860e7f58797945856e49
Richárd <sarkany.richard0@gmail.com>
2015-06-11 17:11:20 +0200

    first

---
+++ .dep/chthreads.o.d
@@ -0,0 +1,114 @@
+build/obj/chthreads.o: ../../Chibios/os/kernel/src/chthreads.c \
+ ../../Chibios/os/kernel/include/ch.h os/chconf.h \
+ ../../Chibios/os/ports/GCC/ARMCMx/chtypes.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/include/stddef.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/include/stdint.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/stdint.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/_ansi.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/newlib.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/config.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/machine/ieeefp.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/features.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/include/stdbool.h \
+ ../../Chibios/os/kernel/include/chlists.h \
+ ../../Chibios/os/ports/GCC/ARMCMx/chcore.h \
+ ../../Chibios/os/ports/GCC/ARMCMx/STM32F1xx/cmparams.h \
+ ../../Chibios/os/ports/GCC/ARMCMx/chcore_v7m.h \
+ ../../Chibios/os/ports/common/ARMCMx/nvic.h \
+ ../../Chibios/os/kernel/include/chsys.h \
+ ../../Chibios/os/kernel/include/chvt.h \
+ ../../Chibios/os/kernel/include/chschd.h \
+ ../../Chibios/os/kernel/include/chsem.h \
+ ../../Chibios/os/kernel/include/chbsem.h \
+ ../../Chibios/os/kernel/include/chmtx.h \
+ ../../Chibios/os/kernel/include/chcond.h \
+ ../../Chibios/os/kernel/include/chevents.h \
+ ../../Chibios/os/kernel/include/chmsg.h \
+ ../../Chibios/os/kernel/include/chmboxes.h \
+ ../../Chibios/os/kernel/include/chmemcore.h \
+ ../../Chibios/os/kernel/include/chheap.h \
+ ../../Chibios/os/kernel/include/chmempools.h \
+ ../../Chibios/os/kernel/include/chthreads.h \
+ ../../Chibios/os/kernel/include/chdynamic.h \
+ ../../Chibios/os/kernel/include/chregistry.h \
+ ../../Chibios/os/kernel/include/chinline.h \
+ ../../Chibios/os/kernel/include/chqueues.h \
+ ../../Chibios/os/kernel/include/chstreams.h \
+ ../../Chibios/os/kernel/include/chfiles.h \
+ ../../Chibios/os/kernel/include/chdebug.h
+
+../../Chibios/os/kernel/include/ch.h:
+
+os/chconf.h:
+
+../../Chibios/os/ports/GCC/ARMCMx/chtypes.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/include/stddef.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/include/stdint.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/stdint.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/_ansi.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/newlib.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/config.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/machine/ieeefp.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/features.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/include/stdbool.h:
+
+../../Chibios/os/kernel/include/chlists.h:
+
+../../Chibios/os/ports/GCC/ARMCMx/chcore.h:
+
+../../Chibios/os/ports/GCC/ARMCMx/STM32F1xx/cmparams.h:
+
+../../Chibios/os/ports/GCC/ARMCMx/chcore_v7m.h:
+
+../../Chibios/os/ports/common/ARMCMx/nvic.h:
+
+../../Chibios/os/kernel/include/chsys.h:
+
+../../Chibios/os/kernel/include/chvt.h:
+
+../../Chibios/os/kernel/include/chschd.h:
+
+../../Chibios/os/kernel/include/chsem.h:
+
+../../Chibios/os/kernel/include/chbsem.h:
+
+../../Chibios/os/kernel/include/chmtx.h:
+
+../../Chibios/os/kernel/include/chcond.h:
+
+../../Chibios/os/kernel/include/chevents.h:
+
+../../Chibios/os/kernel/include/chmsg.h:
+
+../../Chibios/os/kernel/include/chmboxes.h:
+
+../../Chibios/os/kernel/include/chmemcore.h:
+
+../../Chibios/os/kernel/include/chheap.h:
+
+../../Chibios/os/kernel/include/chmempools.h:
+
+../../Chibios/os/kernel/include/chthreads.h:
+
+../../Chibios/os/kernel/include/chdynamic.h:
+
+../../Chibios/os/kernel/include/chregistry.h:
+
+../../Chibios/os/kernel/include/chinline.h:
+
+../../Chibios/os/kernel/include/chqueues.h:
+
+../../Chibios/os/kernel/include/chstreams.h:
+
+../../Chibios/os/kernel/include/chfiles.h:
+
+../../Chibios/os/kernel/include/chdebug.h:
---
+++ .dep/mmc_spi.o.d
@@ -0,0 +1,248 @@
+build/obj/mmc_spi.o: ../../Chibios/os/hal/src/mmc_spi.c \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/string.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/_ansi.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/newlib.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/config.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/machine/ieeefp.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/features.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/reent.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/_ansi.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/_types.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/machine/_types.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/machine/_default_types.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/lock.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/include/stddef.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/string.h \
+ ../../Chibios/os/kernel/include/ch.h os/chconf.h \
+ ../../Chibios/os/ports/GCC/ARMCMx/chtypes.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/include/stdint.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/stdint.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/include/stdbool.h \
+ ../../Chibios/os/kernel/include/chlists.h \
+ ../../Chibios/os/ports/GCC/ARMCMx/chcore.h \
+ ../../Chibios/os/ports/GCC/ARMCMx/STM32F1xx/cmparams.h \
+ ../../Chibios/os/ports/GCC/ARMCMx/chcore_v7m.h \
+ ../../Chibios/os/ports/common/ARMCMx/nvic.h \
+ ../../Chibios/os/kernel/include/chsys.h \
+ ../../Chibios/os/kernel/include/chvt.h \
+ ../../Chibios/os/kernel/include/chschd.h \
+ ../../Chibios/os/kernel/include/chsem.h \
+ ../../Chibios/os/kernel/include/chbsem.h \
+ ../../Chibios/os/kernel/include/chmtx.h \
+ ../../Chibios/os/kernel/include/chcond.h \
+ ../../Chibios/os/kernel/include/chevents.h \
+ ../../Chibios/os/kernel/include/chmsg.h \
+ ../../Chibios/os/kernel/include/chmboxes.h \
+ ../../Chibios/os/kernel/include/chmemcore.h \
+ ../../Chibios/os/kernel/include/chheap.h \
+ ../../Chibios/os/kernel/include/chmempools.h \
+ ../../Chibios/os/kernel/include/chthreads.h \
+ ../../Chibios/os/kernel/include/chdynamic.h \
+ ../../Chibios/os/kernel/include/chregistry.h \
+ ../../Chibios/os/kernel/include/chinline.h \
+ ../../Chibios/os/kernel/include/chqueues.h \
+ ../../Chibios/os/kernel/include/chstreams.h \
+ ../../Chibios/os/kernel/include/chfiles.h \
+ ../../Chibios/os/kernel/include/chdebug.h \
+ ../../Chibios/os/hal/include/hal.h os/board/board.h os/halconf.h \
+ os/mcuconf.h ../../Chibios/os/hal/platforms/STM32F1xx/hal_lld.h \
+ ../../Chibios/os/hal/platforms/STM32/stm32.h \
+ ../../Chibios/os/hal/platforms/STM32F1xx/stm32f10x.h \
+ ../../Chibios/os/ports/common/ARMCMx/CMSIS/include/core_cm3.h \
+ ../../Chibios/os/ports/common/ARMCMx/CMSIS/include/core_cmInstr.h \
+ ../../Chibios/os/ports/common/ARMCMx/CMSIS/include/core_cmFunc.h \
+ ../../Chibios/os/hal/platforms/STM32F1xx/hal_lld_f103.h \
+ ../../Chibios/os/hal/platforms/STM32F1xx/stm32_isr.h \
+ ../../Chibios/os/hal/platforms/STM32F1xx/stm32_dma.h \
+ ../../Chibios/os/hal/platforms/STM32F1xx/stm32_rcc.h \
+ ../../Chibios/os/hal/include/io_channel.h \
+ ../../Chibios/os/hal/include/io_block.h \
+ ../../Chibios/os/hal/include/mmcsd.h ../../Chibios/os/hal/include/tm.h \
+ ../../Chibios/os/hal/include/pal.h \
+ ../../Chibios/os/hal/platforms/STM32/GPIOv1/pal_lld.h \
+ ../../Chibios/os/hal/include/adc.h \
+ ../../Chibios/os/hal/platforms/STM32F1xx/adc_lld.h \
+ ../../Chibios/os/hal/include/can.h \
+ ../../Chibios/os/hal/platforms/STM32/can_lld.h \
+ ../../Chibios/os/hal/include/ext.h ../../Chibios/os/hal/include/gpt.h \
+ ../../Chibios/os/hal/include/i2c.h ../../Chibios/os/hal/include/icu.h \
+ ../../Chibios/os/hal/include/mac.h ../../Chibios/os/hal/include/pwm.h \
+ ../../Chibios/os/hal/include/rtc.h ../../Chibios/os/hal/include/serial.h \
+ ../../Chibios/os/hal/platforms/STM32/USARTv1/serial_lld.h \
+ ../../Chibios/os/hal/include/sdc.h ../../Chibios/os/hal/include/spi.h \
+ ../../Chibios/os/hal/platforms/STM32/SPIv1/spi_lld.h \
+ ../../Chibios/os/hal/include/uart.h ../../Chibios/os/hal/include/usb.h \
+ ../../Chibios/os/hal/include/mmc_spi.h \
+ ../../Chibios/os/hal/include/serial_usb.h
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/string.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/_ansi.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/newlib.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/config.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/machine/ieeefp.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/features.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/reent.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/_ansi.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/_types.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/machine/_types.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/machine/_default_types.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/lock.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/include/stddef.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/string.h:
+
+../../Chibios/os/kernel/include/ch.h:
+
+os/chconf.h:
+
+../../Chibios/os/ports/GCC/ARMCMx/chtypes.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/include/stdint.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/stdint.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.7\ 2014q2\bin\../lib/gcc/arm-none-eabi/4.7.4/include/stdbool.h:
+
+../../Chibios/os/kernel/include/chlists.h:
+
+../../Chibios/os/ports/GCC/ARMCMx/chcore.h:
+
+../../Chibios/os/ports/GCC/ARMCMx/STM32F1xx/cmparams.h:
+
+../../Chibios/os/ports/GCC/ARMCMx/chcore_v7m.h:
+
+../../Chibios/os/ports/common/ARMCMx/nvic.h:
+
+../../Chibios/os/kernel/include/chsys.h:
+
+../../Chibios/os/kernel/include/chvt.h:
+
+../../Chibios/os/kernel/include/chschd.h:
+
+../../Chibios/os/kernel/include/chsem.h:
+
+../../Chibios/os/kernel/include/chbsem.h:
+
+../../Chibios/os/kernel/include/chmtx.h:
+
+../../Chibios/os/kernel/include/chcond.h:
+
+../../Chibios/os/kernel/include/chevents.h:
+
+../../Chibios/os/kernel/include/chmsg.h:
+
+../../Chibios/os/kernel/include/chmboxes.h:
+
+../../Chibios/os/kernel/include/chmemcore.h:
+
+../../Chibios/os/kernel/include/chheap.h:
+
+../../Chibios/os/kernel/include/chmempools.h:
+
+../../Chibios/os/kernel/include/chthreads.h:
+
+../../Chibios/os/kernel/include/chdynamic.h:
+
+../../Chibios/os/kernel/include/chregistry.h:
+
+../../Chibios/os/kernel/include/chinline.h:
+
+../../Chibios/os/kernel/include/chqueues.h:
+
+../../Chibios/os/kernel/include/chstreams.h:
+
+../../Chibios/os/kernel/include/chfiles.h:
+
+../../Chibios/os/kernel/include/chdebug.h:
+
+../../Chibios/os/hal/include/hal.h:
+
+os/board/board.h:
+
+os/halconf.h:
+
+os/mcuconf.h:
+
+../../Chibios/os/hal/platforms/STM32F1xx/hal_lld.h:
+
+../../Chibios/os/hal/platforms/STM32/stm32.h:
+
+../../Chibios/os/hal/platforms/STM32F1xx/stm32f10x.h:
+
+../../Chibios/os/ports/common/ARMCMx/CMSIS/include/core_cm3.h:
+
+../../Chibios/os/ports/common/ARMCMx/CMSIS/include/core_cmInstr.h:
+
+../../Chibios/os/ports/common/ARMCMx/CMSIS/include/core_cmFunc.h:
+
+../../Chibios/os/hal/platforms/STM32F1xx/hal_lld_f103.h:
+
+../../Chibios/os/hal/platforms/STM32F1xx/stm32_isr.h:
+
+../../Chibios/os/hal/platforms/STM32F1xx/stm32_dma.h:
+
+../../Chibios/os/hal/platforms/STM32F1xx/stm32_rcc.h:
+
+../../Chibios/os/hal/include/io_channel.h:
+
+../../Chibios/os/hal/include/io_block.h:
+
+../../Chibios/os/hal/include/mmcsd.h:
+
+../../Chibios/os/hal/include/tm.h:
+
+../../Chibios/os/hal/include/pal.h:
+
+../../Chibios/os/hal/platforms/STM32/GPIOv1/pal_lld.h:
+
+../../Chibios/os/hal/include/adc.h:
+
+../../Chibios/os/hal/platforms/STM32F1xx/adc_lld.h:
+
+../../Chibios/os/hal/include/can.h:
+
+../../Chibios/os/hal/platforms/STM32/can_lld.h:
+
+../../Chibios/os/hal/include/ext.h:
+
+../../Chibios/os/hal/include/gpt.h:
+
+../../Chibios/os/hal/include/i2c.h:
+
+../../Chibios/os/hal/include/icu.h:
+
+../../Chibios/os/hal/include/mac.h:
+
+../../Chibios/os/hal/include/pwm.h:
+
+../../Chibios/os/hal/include/rtc.h:
+
+../../Chibios/os/hal/include/serial.h:
+
+../../Chibios/os/hal/platforms/STM32/USARTv1/serial_lld.h:
+
+../../Chibios/os/hal/include/sdc.h:
+
+../../Chibios/os/hal/include/spi.h:
+
+../../Chibios/os/hal/platforms/STM32/SPIv1/spi_lld.h:
+
+../../Chibios/os/hal/include/uart.h:
+
+../../Chibios/os/hal/include/usb.h:
+
+../../Chibios/os/hal/include/mmc_spi.h:
+
+../../Chibios/os/hal/include/serial_usb.h:
